38ac47c989e89f66dc4616910f18cc2d5e055e5a
Roger Stafford <rogerspianocat@gmail.com>
2021-03-18 17:27:16 +0000

    added some bits

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
-def main():
-  print('You rolled a die')
 
+
+def main():
+    import random
+    dice_rolls=3
+    dsum=0
+    for i in range(0,dice_rolls):
+        roll = random.randint(1,6)
+        dsum+=roll
+        print(f'You rolled a {roll} die')
+        print(f'sum={dsum}')
 if __name__== "__main__":
-  main()
+    main()
